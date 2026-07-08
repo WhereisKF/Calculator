ead0d3470d39670dcd27917676d6693b03d425bf
WhereisKF <fmb659@gmail.com>
2026-07-08 06:38:58 +0100

    Update PayPal API endpoint URLs in script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function closePaymentPopup() {
 
 paypal.Buttons({
     createOrder: async function () {
-        const response = await fetch("https://calculator-backend-xxxx.onrender.com/create-order", {
+        const response = await fetch("https://calculator-backend-8m4i.onrender.com", {
             method: "POST"
         });
 
@@ -37,7 +37,7 @@ paypal.Buttons({
     },
 
     onApprove: async function (data) {
-        const response = await fetch("https://calculator-backend-xxxx.onrender.com/capture-order", {
+        const response = await fetch("https://calculator-backend-8m4i.onrender.com", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
